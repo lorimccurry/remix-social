6ba271aee76365fc29dec25a15ac89da371f72b6
Lori McCurry <lori@softwareforgood.com>
2022-07-05 15:34:59 -0500

    add user and connect to post

--- app/components/Post/Post.tsx
+++ app/components/Post/Post.tsx
@@ -1,9 +1,10 @@
 import type { Props } from './types';
 
-function Post({ title, children }: Props) {
+function Post({ title, authorName, children }: Props) {
   return (
     <div className="flex flex-col p-6 max-w-md border rounded">
       {title && <h2 className="font-bold text-3xl text-gray-900">{title}</h2>}
+      {authorName && <p className="text-gray-700 italic">{authorName}</p>}
       <p className="mt-4 text-lg text-gray-900">{children}</p>
     </div>
   );
